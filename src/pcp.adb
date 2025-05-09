f73d4521140026c2197bf4e606c68fba473a0224
Davide Ceresa <ceredavide@live.it>
2025-05-09 15:01:03 +0200

    feat: improved program GUI and add new exercises

--- src/pcp.adb
+++ src/pcp.adb
@@ -11,7 +11,8 @@ with Show_Simple_Contract;
 with Show_Simple_Postcondition;
 with Show_Simple_Precondition;
 with Sum_Two_Numbers;
-
+with Set_Diff;
+with Quadratic_Solver; 
 procedure PCP is
    pragma Assertion_Policy (Check);
    Choice : Character;
@@ -29,44 +30,60 @@ procedure PCP is
             Sum_Two_Numbers;
 
          when '3' =>
-            Fibonacci;
-
-         when '4' =>
             Show_Simple_Contract;
 
-         when '5' =>
+         when '4' =>
             Show_Simple_Precondition;
 
-         when '6' =>
+         when '5' =>
             Show_Simple_Postcondition;
 
-         when '7' =>
+         when '6' =>
             Int_Sqrt_Contract;
 
-         when '8' =>
+         when '7' =>
             Hello_Tasks;
 
-         when '9' =>
+         when '8' =>
             Show_Predicates;
+
+         when 'A' | 'a' =>
+            Fibonacci;
+         
+         when 'B' | 'b' =>
+            Set_Diff;
+         
+         when 'C' | 'c' =>
+            Quadratic_Solver;
 
          when others =>
             Put_Line ("Invalid choice. Please try again.");
+
       end case;
    end Handle_User_Choice;
 begin
    New_Line;
    loop
       Put_Line ("Select an example to run:");
+      New_Line;
+      Put_Line ("EXAMPLES:");
+      New_Line;
       Put_Line ("0. Hello World");
       Put_Line ("1. Check Positive");
       Put_Line ("2. Sum Two Numbers");
-      Put_Line ("3. Fibonacci");
-      Put_Line ("4. Show Simple Contract");
-      Put_Line ("5. Show Simple Pre Condition");
-      Put_Line ("6. Show Simple Post Condition");
-      Put_Line ("7. Int Square Root Contract");
-      Put_Line ("8. Hello Tasks");
-      Put_Line ("9. Show Predicates");
+      Put_Line ("3. Show Simple Contract");
+      Put_Line ("4. Show Simple Pre Condition");
+      Put_Line ("5. Show Simple Post Condition");
+      Put_Line ("6. Int Square Root Contract");
+      Put_Line ("7. Hello Tasks");
+      Put_Line ("8. Show Predicates");
+      New_Line;
+      Put_Line ("EXERCISES:");
+      New_Line;
+      Put_Line ("A. Fibonacci (SW03-EX1)");
+      Put_Line ("B. Array Difference (SW04-EX2)");
+      Put_Line ("C. Quadratic Solver (SW05-EX5)");
+      New_Line;
       Put_Line ("Q. Quit");
       New_Line;
 
@@ -81,6 +98,8 @@ begin
          when others =>
             begin
                Handle_User_Choice (Choice);
+               New_Line;
+               Put_Line ("Example Completed!");
             exception
                when E : others =>
                   Put_Line
